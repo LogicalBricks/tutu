a207611519dad73cac00b30b48babdffa533e7f0
Hermes Ojeda Ruiz <hermes.ojeda@logicalbricks.com>
2012-10-16 15:48:26 -0500

    Initial commit with rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutu::Application.config.secret_token = '44d9bccd95d834c3a79897d3b7c7cdac26d09cd449e588fe125feceb3dab89d324257e448a34b16a8c125540e96254a3ef2bc225a27a1d3bac632c0add776d25'
